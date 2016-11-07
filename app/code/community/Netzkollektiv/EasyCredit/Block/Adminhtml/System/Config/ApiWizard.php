98e7544a560910295038343274992065fa32f405
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2016-11-07 17:40:06 +0100

    cleanup

--- app/code/community/Netzkollektiv/EasyCredit/Block/Adminhtml/System/Config/ApiWizard.php
+++ app/code/community/Netzkollektiv/EasyCredit/Block/Adminhtml/System/Config/ApiWizard.php
@@ -1,31 +1,6 @@
 <?php
 /**
- * Magento
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magento.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magento.com for more information.
- *
- * @category    Mage
- * @package     Mage_Paypal
- * @copyright  Copyright (c) 2006-2016 X.commerce, Inc. and affiliates (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
-
-/**
- * Custom renderer for PayPal API credentials wizard popup
+ * Custom renderer for EasyCredit API credentials wizard
  */
 class Netzkollektiv_EasyCredit_Block_Adminhtml_System_Config_ApiWizard extends Mage_Adminhtml_Block_System_Config_Form_Field
 {
@@ -70,7 +45,6 @@ class Netzkollektiv_EasyCredit_Block_Adminhtml_System_Config_ApiWizard extends M
         $elementHtmlId = $element->getHtmlId();
         $this->addData(array_merge(
             $this->_getButtonData($elementHtmlId, $originalData),
-            $this->_getSandboxButtonData($elementHtmlId, $originalData)
         ));
         return $this->_toHtml();
     }
@@ -91,22 +65,6 @@ class Netzkollektiv_EasyCredit_Block_Adminhtml_System_Config_ApiWizard extends M
             'button_label' => Mage::helper('easycredit')->__($originalData['button_label']),
             'button_url'   => $originalData['button_url'],
             'html_id' => $elementHtmlId,
-        );
-    }
-
-    /**
-     * Prepare sandbox button data
-     *
-     * @param string $elementHtmlId
-     * @param array $originalData
-     * @return array
-     */
-    protected function _getSandboxButtonData($elementHtmlId, $originalData)
-    {
-        return array(
-            'sandbox_button_label' => Mage::helper('paypal')->__($originalData['sandbox_button_label']),
-            'sandbox_button_url'   => $originalData['sandbox_button_url'],
-            'sandbox_html_id' => 'sandbox_' . $elementHtmlId,
         );
     }
 }
