df0aeb192628c09fed75309b8b247c935d817714
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2018-10-09 11:20:28 +0200

    replaced widget against non-bootstrap widget; added grunt for compiled ressources

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+
+    var baseDir = 'src/skin/frontend/base/default';
+    var baseDirJs = baseDir+'/js/easycredit'; 
+    var baseDirCss = baseDir+'/css/easycredit';
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.initConfig({
+      uglify: {
+        easycredit: {
+          options: {
+            sourceMap: true,
+            sourceMapName: baseDir+'/js/easycredit.min.js.map'
+          },
+          files: {
+            [baseDirJs+'/easycredit.min.js']: [
+                baseDirJs+'/src/easycredit-modal.js',
+                baseDirJs+'/src/easycredit-widget.js',
+                baseDirJs+'/src/easycredit-frontend.js'
+            ]
+          }
+        }
+      },
+      cssmin: {
+          options: {
+            mergeIntoShorthands: false,
+            roundingPrecision: -1
+          },
+          easycredit: {
+            files: {
+              [baseDirCss+'/easycredit.min.css']: [
+                baseDirCss+'/src/easycredit-modal.css',
+                baseDirCss+'/src/easycredit-widget.css',
+                baseDirCss+'/src/easycredit-frontend.css'
+              ]
+            }
+          }
+      }
+
+    });
+    grunt.registerTask('default', ['uglify','cssmin']);
+}
