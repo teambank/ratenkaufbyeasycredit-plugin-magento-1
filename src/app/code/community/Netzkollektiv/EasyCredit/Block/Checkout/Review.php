265eb04884bd295ea9c20d4141d6f28b17500b25
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2020-05-12 13:32:20 +0200

    removed pre contract information url; removed redepmtion plan

--- src/app/code/community/Netzkollektiv/EasyCredit/Block/Checkout/Review.php
+++ src/app/code/community/Netzkollektiv/EasyCredit/Block/Checkout/Review.php
@@ -9,13 +9,4 @@
  */
 
 class Netzkollektiv_EasyCredit_Block_Checkout_Review extends Mage_Checkout_Block_Onepage_Progress {
-    public function getPreContractInformationUrl() {
-        return $this->getQuote()->getPayment()
-            ->getAdditionalInformation('pre_contract_information_url');
-    }
-
-    public function getRedemptionPlan() {
-        return $this->getQuote()->getPayment()
-            ->getAdditionalInformation('redemption_plan');
-    }
 }
