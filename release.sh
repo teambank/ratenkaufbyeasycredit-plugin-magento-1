5bc09b9d8670f7c8de3233321c225243c0e95af9
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2020-05-13 18:16:09 +0200

    updated merchant interface repo

--- release.sh
+++ release.sh
@@ -3,13 +3,17 @@ base="$(cd "$(dirname "$0")"; pwd)";
 rm -r build/*
 
 mkdir -p build/lib/Netzkollektiv/EasyCreditApi
+mkdir -p build/skin/adminhtml/base/default/easycredit
+
+version=$(php -r "echo current(simplexml_load_file('src/app/code/community/Netzkollektiv/EasyCredit/etc/config.xml')->modules)->version;")
 
 cp -r src/* build/
 cp -r module-api/EasyCreditApi/* build/lib/Netzkollektiv/EasyCreditApi/
+rsync -r --exclude index.html merchant-interface/dist/* build/skin/adminhtml/base/default/easycredit/
 (cd build; tar cvf ext.tar *)
 
 php ../MagentoTarToConnect/magento-tar-to-connect.php package-config.php 
-(cd src/ && zip -r - *) > dist/file-release.zip
+(cd src/ && zip -r - *) > dist/m1-easycredit-$version.zip
 
 rm -r dist/build
 
